fe5c4dec854accd5c496c3a61633a52c9fe2476f
AlexReverse <mr.evteev.1998@gmail.com>
2023-12-24 17:48:18 +0300

    Day 2

---
+++ src/main/java/intercore/InterCoreApplication.java
@@ -0,0 +1,11 @@
+package intercore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterCoreApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(InterCoreApplication.class, args);
+    }
+}
---
+++ src/main/java/intercore/InterCoreApplicationTest.java
@@ -0,0 +1,10 @@
+package intercore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class InterCoreApplicationTest{
+    @Test
+    public void contextLoads(){ }
+}

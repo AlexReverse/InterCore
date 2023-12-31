cb10a5040337394f149dad0f060233b9665f22b6
AlexReverse <mr.evteev.1998@gmail.com>
2023-12-31 15:36:14 +0300

    Day 7

---
+++ src/main/java/intercore/web/InformationController.java
@@ -0,0 +1,37 @@
+package intercore.web;
+
+import intercore.MemberInformation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
+
+import javax.validation.Valid;
+
+@Slf4j
+@Controller
+@RequestMapping("/information")
+@SessionAttributes("memberInformation")
+public class InformationController {
+    @GetMapping("/current")
+    public String informationForm(){
+        return "informationForm";
+    }
+
+    @PostMapping
+    public String processOrder(@Valid MemberInformation memberInformation, Errors errors,
+                               SessionStatus sessionStatus) {
+        if (errors.hasErrors()) {
+            return "informationForm";
+        }
+
+        log.info("Player submitted: {}", memberInformation);
+        sessionStatus.setComplete();
+
+        return "redirect:/";
+    }
+}

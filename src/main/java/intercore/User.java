ed638f7f70a270142b83959c9b1a73d63fbff512
AlexReverse <mr.evteev.1998@gmail.com>
2024-01-10 14:21:46 +0300

    Day 15

--- src/main/java/intercore/User.java
+++ src/main/java/intercore/User.java
@@ -1,9 +1,6 @@
 package intercore;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -19,12 +16,14 @@ import java.util.Collection;
 @RequiredArgsConstructor
 @Data
 @NoArgsConstructor(access = AccessLevel.PRIVATE, force = true)
+@Table(name="UserRepo")
 public class User implements UserDetails {
 
     private static final long serialVersionUID = 1L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "User_ID")
     private Long id;
 
     private final String username;

672e532d99a0eec660a5f1d1528ad5b1b2b3a29f
AlexReverse <mr.evteev.1998@gmail.com>
2024-01-11 23:55:26 +0300

    Day 16

--- src/main/java/intercore/InterCoreApplication.java
+++ src/main/java/intercore/InterCoreApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
 public class InterCoreApplication {
@@ -13,6 +14,7 @@ public class InterCoreApplication {
     }
 
     @Bean
+    @Profile("prod")
     public CommandLineRunner dataLoader(DisciplineRepository repo) {
         return new CommandLineRunner() {
             @Override
--- src/main/java/intercore/data/MemberRepository.java
+++ src/main/java/intercore/data/MemberRepository.java
@@ -2,6 +2,7 @@ package intercore.data;
 
 import intercore.Member;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface MemberRepository  extends CrudRepository<Member, Long> {
+public interface MemberRepository  extends PagingAndSortingRepository<Member, Long>, CrudRepository<Member, Long> {
 }

06216a0736fa25ea20cb76bb628c138179e4dbd3
AlexReverse <mr.evteev.1998@gmail.com>
2024-01-16 21:19:00 +0300

    Day 19

--- src/main/java/intercore/security/SecurityConfig.java
+++ src/main/java/intercore/security/SecurityConfig.java
@@ -1,6 +1,5 @@
 package intercore.security;
 
-import intercore.data.UserRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -9,13 +8,9 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-
-import static org.springframework.security.config.Customizer.withDefaults;
-
 
 @Configuration
 @EnableWebSecurity
@@ -29,14 +24,17 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
                 .authorizeHttpRequests((authz) -> authz
+                        .requestMatchers("static/**").permitAll()
                         .anyRequest().authenticated()
                 )
-                .httpBasic(withDefaults())
                 .formLogin(form -> form
                         .loginPage("/login")
                         .permitAll()
+                        .defaultSuccessUrl("/home")
                 )
-                .csrf((csrf) -> csrf.disable())
+                .csrf((csrf) -> csrf
+                        .ignoringRequestMatchers("/create*")
+                )
                 .build();
     }
 
